346acaa117a97883809ff9a6e91024e5042fc939
Demon3003 <zhuravlyovdima34@gmail.com>
2024-03-04 19:24:01 +0200

    Add widget to data layer. Update schema for dashboard table.

--- backend/api/src/main/java/com/zhurawell/base/api/controllers/dashboards/DashboardsRestController.java
+++ backend/api/src/main/java/com/zhurawell/base/api/controllers/dashboards/DashboardsRestController.java
@@ -3,7 +3,9 @@ package com.zhurawell.base.api.controllers.dashboards;
 
 import com.fasterxml.jackson.databind.node.BigIntegerNode;
 import com.zhurawell.base.data.model.dashboard.Dashboard;
+import com.zhurawell.base.data.model.widget.Widget;
 import com.zhurawell.base.data.repo.dashboard.DashboardRepo;
+import com.zhurawell.base.data.repo.widget.WidgetRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +22,17 @@ public class DashboardsRestController {
     @Autowired
     private DashboardRepo dashboardRepo;
 
+    @Autowired
+    private WidgetRepo widgetRepo;
+
     @GetMapping("/get/{id}")
     public ResponseEntity getDashboardById(@PathVariable("id") BigInteger id) {
         Dashboard d = dashboardRepo.getById(id);
+        Widget w = widgetRepo.getById(id);
+
+        w.setName("Test Test");
+        w.setData("{\"color\": \"red\", \"size\": \"20x20\"}");
+        widgetRepo.save(w);
         return ResponseEntity.ok().body(d);
     }
 }
